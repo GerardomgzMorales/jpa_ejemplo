ff6c80cfaeada49c1293d2a4163035e6de862636
Gerardo Morales Gómez <moralesgerardomgz@outlook.com>
2021-09-04 23:02:05 -0500

    Se carga una mejora en la parte de la funcionalidad de query -JPA- GMZ

--- src/main/java/mx/historial/com/manager/ManagerEntity.kt
+++ src/main/java/mx/historial/com/manager/ManagerEntity.kt
@@ -1,6 +1,7 @@
 package mx.historial.com.manager
 
 import mx.historial.com.model.Persona
+import javax.persistence.EntityManagerFactory
 import javax.persistence.Persistence
 
 class ManagerEntity {
@@ -8,10 +9,14 @@ class ManagerEntity {
 }
 
 fun main() {
-    val facManager = Persistence.createEntityManagerFactory("ejemploKotlinJPA") //lee la config y sabe como crear
+    val facManager: EntityManagerFactory =
+        Persistence.createEntityManagerFactory("ejemploKotlinJPA") //lee la config y sabe como crear
     val em = facManager.createEntityManager()
 
     println((em.find(Persona::class.java, "1".toLong()) as Persona))
+
+    em.clear()
+    em.close()
 
 
 }
